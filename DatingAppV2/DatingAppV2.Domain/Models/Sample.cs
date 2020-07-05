7914d9822a90ae782705cc91badeec0c33871650
fnmdelacruz <fnmdelacruz@gmail.com>
2020-07-06 00:46:34 +0800

    Adding Sample Model

---
+++ DatingAppV2/DatingAppV2.Domain/Models/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingAppV2.Domain.Models
+{
+    public class Sample
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Descriptiion { get; set; }
+        public string ImagerUrl { get; set; }
+    }
+}
